c291dbae9f3846b7fcbdec41bcd9d3750fcc16ef
lufherna <rojoclifford@gmail.com>
2017-08-02 11:13:38 -0400

    modified index.blade to remove unecessary data

--- Career/resources/views/index.blade.php
+++ Career/resources/views/index.blade.php
@@ -130,7 +130,7 @@
 												<li><a href="#" class="button">More</a></li>
 											</ul>
 										</article>
-										<article>
+										<!-- <article>
 											<a href="#" class="image"><img src="images/pic05.jpg" alt="" /></a>
 											<h3>Feugiat lorem aenean</h3>
 											<p>Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.</p>
@@ -145,7 +145,7 @@
 											<ul class="actions">
 												<li><a href="#" class="button">More</a></li>
 											</ul>
-										</article>
+										</article> -->
 									</div>
 								</section>
 
