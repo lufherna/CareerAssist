94b3a23ecd3390ac88f2cdf28ae7faa91e2b9b8e
lufherna <rojoclifford@gmail.com>
2017-08-02 02:13:45 -0400

    added coding to allow login and registration is now working

--- Career/app/Providers/AppServiceProvider.php
+++ Career/app/Providers/AppServiceProvider.php
@@ -3,6 +3,12 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+// the code below was added and may not work
+// i also jumped into the php.ini file in the C drive
+// and changed the sdo_mysql.dll file
+// if errors come up then add a semi colon to the front
+// of that file name and it'll be back how it was 
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +19,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // this code was added in order to fix a length
+        // error thrown when the migration is ran
+        Schema::defaultStringLength(191);
     }
 
     /**
